2b24e6738d6e7de60138332b39454d3808d5016a
wpichl <walter.pichl@hotmail.com>
2021-12-02 16:30:18 +0100

    finished orderedjobs

--- 03_SourceCode/Katas/OrderedJobs/src/Job.java
+++ 03_SourceCode/Katas/OrderedJobs/src/Job.java
@@ -1,26 +1,16 @@
-public class Job{
+public class Job {
     private String name;
-
-    private Job dependsOn;
-
-    public Job(String name, Job dependsOn){
-        this.name = name;
-        this.dependsOn = dependsOn;
-    }
-    public Job(String name){
+    
+    public Job(String name) {
         this.name = name;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
-    public Job getDependency() {
-        return dependsOn;
-    }
-
-    public boolean isDependent(){
-
-        return false;
+    @Override
+    public String toString() {
+        return this.getName();
     }
 }
--- 03_SourceCode/Katas/OrderedJobs/src/JobsScheduler.java
+++ 03_SourceCode/Katas/OrderedJobs/src/JobsScheduler.java
@@ -3,31 +3,68 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class JobsScheduler {
-    private List<Job> jobs = new LinkedList<Job>();
-    public void registerJob(String jobName){
-        Job job = new Job(jobName);
-        if(!jobs.contains(job)){
-            jobs.add(job);
+    private LinkedList<Job> jobs = new LinkedList<Job>();
+
+    private boolean containsJob(String jobName) {
+        for (Job j : this.jobs) {
+            if(j.getName().equals(jobName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Job getJob(String jobName) {
+        for(Job j : this.jobs) {
+            if(j.getName().equals(jobName)) {
+                return j;
+            }
+        }
+
+        return null;
+    }
+
+    public void registerJob(String jobName) {
+        if(!containsJob(jobName)) {
+            this.jobs.add(new Job(jobName));
         }
     }
+
 
     public void sort() {
 
     }
 
     public String getList() {
-        return jobs;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < this.jobs.size(); i++) {
+            sb.append(this.jobs.get(i));
+        }
+        return sb.toString();
     }
 
     public void registerJob(String dependentJob, String independentJob) {
-        Job job = new Job(independentJob);
-        if(!jobs.contains(job)){
-            jobs.add(job);
+        Job independent = new Job(independentJob);
+        Job dependent = new Job(dependentJob);
+
+        if(!containsJob(independentJob) && !containsJob(dependentJob)) {
+            this.jobs.add(independent);
+            this.jobs.add(dependent);
         }
-        Job job2 = new Job(dependentJob, job);
-        if(!jobs.contains(job2)){
-            jobs.add(job2);
+        else if(!containsJob(dependentJob)) {
+            int idx = this.jobs.indexOf(getJob(independent.getName()));
+            this.jobs.add(idx + 1,dependent);
+        }
+        else if(!containsJob(independentJob)) {
+            int idx = this.jobs.indexOf(getJob(dependent.getName()));
+
+            if(idx == 0) {
+                this.jobs.push(independent);
+            }
+            else {
+                this.jobs.add(idx - 1, independent);
+            }
         }
     }
-
 }
