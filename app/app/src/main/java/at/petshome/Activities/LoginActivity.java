c94b4146b88fcd320fa297664c167e0c15e49134
smur1c <42814394+smur1c@users.noreply.github.com>
2022-05-13 08:43:36 +0200

    latest commit

--- app/app/src/main/java/at/petshome/Activities/LoginActivity.java
+++ app/app/src/main/java/at/petshome/Activities/LoginActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import at.petshome.R;
 import at.petshome.database.Connector;
@@ -23,13 +27,30 @@ public class LoginActivity extends AppCompatActivity {
         EditText password_field = (EditText)findViewById(R.id.login_password);
 
         String email = email_field.getText().toString();
+        email_field.setError(null);
+        password_field.setError(null);
         String password = password_field.getText().toString();
 
-        if (email == null || password == null || email.equals("") || password.equals("")) {
-            System.out.println("Email or password cant be empty!");
-        }
-        else {
 
+
+        if(!regexTesterEmail(email)){
+            email_field.setError("Wrong format");
+        }
+        if(!regexPasswordTester(password)){
+            if(password.length() < 8){
+                password_field.setError("Password too short");
+            }
+            else {
+                password_field.setError("Wrong format");
+            }
+        }
+
+        boolean is_emailValid = regexTesterEmail(email);
+        boolean is_passwordValid = regexPasswordTester(password);
+
+        if(is_emailValid && is_passwordValid){
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
         }
     }
 
@@ -42,4 +63,21 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ResetPasswordActivity.class);
         startActivity(intent);
     }
+
+    public boolean regexTesterEmail(String email){
+        String regex = "^(.+)@(.+)$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public boolean regexPasswordTester(String password){
+        String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
+
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+            Matcher matcher = pattern.matcher(password);
+            return matcher.matches();
+        }
 }
